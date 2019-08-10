52a4da0d90dbe8e3d1357d330becf5c49a387a07
Johnny <johnny90347@gmail.com>
2019-08-10 23:47:43 +0800

    Initial Commit

---
+++ Johnny_WEMOdemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Johnny_WEMOdemo
+//
+//  Created by 梁鑫文 on 2019/8/10.
+//  Copyright © 2019 Johhny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
